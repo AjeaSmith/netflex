c1996abde6cbe75015b30e10ce67f31e609262f6
Ajea Smith <ajeasmith@Ajeas-MBP.attlocal.net>
2019-08-25 15:58:13 -0400

    add build folder

---
+++ build/precache-manifest.5de85391d2b77ecbc8c88ba64d76b664.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "268acdc22956f86e8e2be515ef7ef392",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85deffcdd13505ab4763",
+    "url": "/static/css/main.66db0a18.chunk.css"
+  },
+  {
+    "revision": "f3e3455c376df871f421",
+    "url": "/static/js/2.ab149208.chunk.js"
+  },
+  {
+    "revision": "85deffcdd13505ab4763",
+    "url": "/static/js/main.060f8c94.chunk.js"
+  },
+  {
+    "revision": "c6a7d2ed1e2c528d86e6",
+    "url": "/static/js/runtime~main.bdd2b5ae.js"
+  }
+]);
